a42a2110f52bf4e3c3806e18744928fef8a531b0
Anirudh Khade <anirudh24io010@satiengg.in>
2024-05-10 14:59:30 -0700

    partials folder

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+
+});
+
+router.post('/register', function(req, res, next) {
+  res.send('/profile');
+});
+
+module.exports = router;
